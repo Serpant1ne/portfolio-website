cf5d0c615b961acce8fb8b50d6b94153fc93b77d
Konstantin Gartsman <kosgar228@yandex.ru>
2022-11-06 14:52:56 +0300

    made 320px

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'ssm': '320px',
+        
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+
+    },
+
+
   },
   plugins: [],
 }
